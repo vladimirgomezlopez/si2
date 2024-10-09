ee28ebfb8422c77bb4b40373fca6accc1e4097a1
Tais20249 <202000236@est.umss.edu>
2024-10-09 07:41:43 -0400

    Update crear_tabla_documentos.sql

--- crear_tabla_documentos.sql
+++ crear_tabla_documentos.sql
@@ -2,6 +2,6 @@ CREATE TABLE documentos (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     contenido TEXT,
-    usuario_id INTEGER REFERENCES usuario(id),
+    usuario_id INTEGER REFERENCES usuarios(id),
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
