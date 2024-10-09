ef5b6bf24a49f1a0be2257b8a696f770a516877e
Tais20249 <202000236@est.umss.edu>
2024-10-09 07:41:01 -0400

    Update crear_tabla_comentarios.sql

--- crear_tabla_comentarios.sql
+++ crear_tabla_comentarios.sql
@@ -1,6 +1,6 @@
 CREATE TABLE comentarios (
     id SERIAL PRIMARY KEY,
-    documento_id INTEGER REFERENCES documentos(id),
+    documento_id INTEGER REFERENCES documento(id),
     usuario_id INTEGER REFERENCES usuarios(id),
     contenido TEXT NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
