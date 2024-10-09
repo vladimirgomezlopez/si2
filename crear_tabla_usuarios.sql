6ec1995527f271a2b8e60d90b42ed8b88db0249e
Tais20249 <202000236@est.umss.edu>
2024-10-09 07:42:27 -0400

    Update crear_tabla_usuarios.sql

--- crear_tabla_usuarios.sql
+++ crear_tabla_usuarios.sql
@@ -2,5 +2,5 @@ CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
     nombre_usuario VARCHAR(50) UNIQUE NOT NULL,
     contrasena VARCHAR(255) NOT NULL,
-    emails VARCHAR(100) UNIQUE NOT NULL
+    email VARCHAR(100) UNIQUE NOT NULL
 );
